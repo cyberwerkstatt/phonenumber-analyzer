68f116ab40164a1b806f73cae8a76832fa8d3059
cyberwerkstatt <seslija.sasha@gmail.com>
2022-11-14 15:02:17 +0100

    adapting new code

--- analyzer.py
+++ analyzer.py
@@ -1,17 +1,19 @@
 from twilio.rest import Client
 account_sid = 'your_sid'
-auth_token = 'your_token'
+auth_token = 'your auth_token'
 client = Client(account_sid, auth_token)
 
-try:
-    with open("./testphone.txt") as number:
-        for line in number:
-            phone_number = client.lookups.phone_numbers(line).fetch(type='carrier')
+
+with open("<your dierctory with txt-file") as number:
+    for line in number:
+        try:
+            phone_number = client.lookups.phone_numbers(line.strip()).fetch(type='carrier')
             carrier = phone_number.carrier["name"]
             country = phone_number.country_code
             number = line
             print("Nummer:",number + " Anbieter:",carrier," Land:",country,"\n")
-except KeyboardInterrupt and Exception:
-    print("Aborting......")
-    print("done!")
+        except:
+            continue
+        
+print("done")
         
